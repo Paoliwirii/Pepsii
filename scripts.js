ac0c4c8cbbffa3372d93189c59aab1f0d902f931
Paoliwirii <paoliiialej@gmail.com>
2025-12-09 20:26:07 -0600

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,49 @@
+// 1. Seleccionamos las secciones y los puntos
+    const sections = document.querySelectorAll('section');
+    const navDots = document.querySelectorAll('.side-nav a');
+
+    // 2. Configuración del "Observador" (Detecta qué se ve en pantalla)
+    const observerOptions = {
+        root: document.querySelector('.scroll-container'), // Quién tiene el scroll
+        threshold: 0.5 // Se activa cuando el 50% de la sección es visible
+    };
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                // Removemos 'active' de todos los puntos
+                navDots.forEach(dot => dot.classList.remove('active'));
+                
+                // Buscamos el punto que corresponde a esta sección (usando el href)
+                const id = entry.target.getAttribute('id');
+                const activeDot = document.querySelector(`.side-nav a[href="#${id}"]`);
+                
+                // Si existe, le ponemos la clase active
+                if (activeDot) {
+                    activeDot.classList.add('active');
+                }
+            }
+        });
+    }, observerOptions);
+
+    // 3. Ponemos al observador a vigilar cada sección
+    sections.forEach(section => {
+        observer.observe(section);
+    });
+
+        /* =========================================
+       1. LÓGICA DEL LOADER (NUEVO)
+       ========================================= */
+    window.addEventListener('load', function() {
+        // Esperamos 1.5 segundos para que se vea la animación del logo
+        setTimeout(function() {
+            // Agregamos la clase 'loaded' al body para activar el CSS
+            document.body.classList.add('loaded');
+            
+            // Opcional: Eliminar el loader del HTML después de la animación para limpiar
+            setTimeout(() => {
+                document.getElementById('loader-wrapper').style.display = 'none';
+            }, 1500); // Espera a que termine la animación del telón
+            
+        }, 1500); // Tiempo que dura el logo palpitando antes de abrirse
+    });
